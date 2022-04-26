d7faa7ed039dc04dabafac6a11126f1876de442a
Mark <Mark@SurfacePro>
2022-04-26 16:00:49 +0100

    Report evaluation result.

--- SquareCheck/wwwroot/js/site.js
+++ SquareCheck/wwwroot/js/site.js
@@ -1,15 +1,21 @@
 ﻿let txtInput = $("#txtInput");
 let btnEvaluate = $("#btnEvaluate");
+let lblOutput = $("#lblResult");
 
 btnEvaluate.click(function () {
     $.ajax({
         type: "GET",
         url: "Home/IsSquareNumber/" + txtInput.val(),
         success: function (response) {
-            console.log(response);
+            btnEvaluate.prop('disabled', true);
+            lblOutput.text(response.message);
+            lblOutput.css("color", response.isSquare ? "#00FF00" : "#FF0000");
+            lblOutput.show();
+            setTimeout(() => lblOutput.fadeOut(500), 1500);
+            setTimeout(() => btnEvaluate.prop('disabled', false), 2000);
         },
         error: function (response) {
-            console.log(response);
+            alert(response);
         }
     });
 });

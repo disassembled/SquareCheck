6e7df983d5a53cec3fef2760d6e657132d933b5f
Mark <Mark@SurfacePro>
2022-04-26 15:02:34 +0100

    Take input and confirm.

--- SquareCheck/wwwroot/js/site.js
+++ SquareCheck/wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿let txtInput = $("#txtInput");
+let btnEvaluate = $("#btnEvaluate");
 
-// Write your JavaScript code.
+btnEvaluate.click(function () {
+    console.log(txtInput.val());
+});
